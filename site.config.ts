66f9b38228391101cc612ca85adc5ce78ec5805f
Li Han <69972423+bionicotaku@users.noreply.github.com>
2023-03-26 04:07:45 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '6943adecda9244de9431631b7e3fba70',
+  rootNotionPageId: 'Bionic-Otaku-6943adecda9244de9431631b7e3fba70',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
